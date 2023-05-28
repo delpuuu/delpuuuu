3e4fa0925c00473dd71fddc6af84f6a1ffb801fa
Nicolas <soynicolasdelpino@gmail.com>
2023-05-28 00:18:43 -0300

    antes de los cambios

---
+++ src/componentes.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+function Grid(){
+    return(
+    <body>
+    
+    <div class="cajas">
+        <div class="c1">
+            <h1>ALUMNOS</h1>
+
+        </div>
+        <div class="c2">
+            <h1>MENU</h1>
+        </div>
+        <div class="c3">
+            <h1>BARRA LATERAL</h1>
+        </div>
+        <div class="c4">
+            <h1>FOOTER</h1>
+        </div>
+    </div>
+
+</body>
+    )
+}
+export default Grid;
