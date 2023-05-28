50f510b07c24a3a8dd31e81738f326efbb0a225d
Nicolas <soynicolasdelpino@gmail.com>
2023-05-28 20:05:25 -0300

    ya tenemos lista

--- src/componentes.js
+++ src/componentes.js
@@ -1,26 +1,29 @@
 import React from 'react'
+import item from './lista'
+import ListaMiembros from './lista';
 
 function Grid(){
     return(
-    <body>
-    
     <div class="cajas">
         <div class="c1">
-            <h1>ALUMNOS</h1>
+            <h1>cabercera</h1>
 
         </div>
         <div class="c2">
             <h1>MENU</h1>
         </div>
         <div class="c3">
-            <h1>BARRA LATERAL</h1>
+            <h1>contenido</h1> 
+            <ListaMiembros/>
         </div>
         <div class="c4">
-            <h1>FOOTER</h1>
+            <h1>barra lateral</h1>
+        </div>
+        <div class='c5'>
+            <h1>footer</h1>
         </div>
     </div>
+)
 
-</body>
-    )
 }
 export default Grid;
---
+++ src/lista.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './index.css' 
+
+function Item({ nombre, miembros }) {
+    return miembros ? <li className="item">{nombre} ✔️</li> : <li>{nombre} ❌</li>;
+  }
+  
+  function ListaMiembros() {
+    return (
+      <div className="list">
+        <h2>Lista de miembros</h2>
+        <ul class="listaNazi">
+          <Item miembros={true} nombre={"del Pino Nicolas"} />
+          <Item miembros={true} nombre={"Posadas Agustin"} />
+          <Item miembros={true} nombre={"Reartes Bruno"} />
+          <Item miembros={true} nombre={"Goku"} />
+          <Item miembros={false} nombre={"Naruto"} />
+          <Item miembros={false} nombre={"Kaneki"} />
+          <Item miembros={false} nombre={"Nacho"} />
+        </ul>
+      </div>    
+    );
+  }
+  
+  export default ListaMiembros;
